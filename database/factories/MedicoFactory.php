bd074e8f1eee9fe43be6e2abe90fd576cd4f220b
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2023-08-02 00:12:56 -0300

    create factory Medico

---
+++ database/factories/MedicoFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Medico;
+use App\Models\Cidade;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Medico>
+ */
+class MedicoFactory extends Factory
+{
+    protected $model = Medico::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nome' => $this->faker->name,
+            'especialidade' => $this->faker->randomElement(['Cardiologista', 'Pediatra', 'Dermatologista', 'Ginecologia', 'Obstetrícia', 'Ortopedia']),
+            'cidade_id' => Cidade::factory()->create()->id,
+        ];
+    }
+}
