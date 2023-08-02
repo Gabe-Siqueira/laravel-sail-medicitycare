39d3fa193db0e67d19902cb8aa6faa85bb299e91
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2023-08-02 02:49:35 -0300

    Adicionando 'CidadeSeeder' no script -> DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            CidadeSeeder::class,
             MedicoPacienteSeeder::class,
         ]);
     }
