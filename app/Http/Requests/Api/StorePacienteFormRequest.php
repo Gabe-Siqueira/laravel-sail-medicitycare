81f8c99e6b938973906d77f69ffd38fd316438c1
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2023-08-03 00:11:05 -0300

    create validação insert 'Paciente' -> StorePacienteFormRequest

---
+++ app/Http/Requests/Api/StorePacienteFormRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class StorePacienteFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|max:100',
+            'cpf' => 'required|string|max:20',
+            'celular' => 'required|string|max:20',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json($validator->errors(), JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
